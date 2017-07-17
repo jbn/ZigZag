ef98f6199058fae3bc0cb7c751a1f3b67becac4d
John B Nelson <jbn@artifexdeus.com>
2017-07-17 12:53:44 -0700

    Bump version

--- zigzag/__init__.py
+++ zigzag/__init__.py
@@ -7,7 +7,7 @@ __uri__ = "https://github.com/jbn/ZigZag"
 __doc__ = __description__ + " <" + __uri__ + ">"
 __license__ = " BSD-3-Clause"
 __copyright__ = "Copyright (c) 2017 John Bjorn Nelson"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __author__ = "John Bjorn Nelson"
 __email__ = "jbn@abreka.com"
 
