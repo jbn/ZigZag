0f1e4cf69e263d600d8ad4f35c51f3abc1d99739
John B Nelson <jbn@abreka.com>
2022-07-31 14:26:15 -0700

    Close issue #20 (#22)
    * fix point error
    
    * Work on issue #13
    
    * FIX pandas compat issue #2
    
    * Drop 3.7 support
    
    * Update changes for 0.3.0 release
    
    * Remove garbage from __init__.py (closes #20)
    
    Co-authored-by: tianyebj <tianyebj@gmail.com>

--- zigzag/__init__.py
+++ zigzag/__init__.py
@@ -1,15 +1,4 @@
 from zigzag.core import *
 
-
-__title__ = "zigzag"
-__description__ = "Package for finding peaks and valleys of time series."
-__uri__ = "https://github.com/jbn/ZigZag"
-__doc__ = __description__ + " <" + __uri__ + ">"
-__license__ = " BSD-3-Clause"
-__copyright__ = "Copyright (c) 2017-2022 John Bjorn Nelson"
-__version__ = "0.2.2"
-__author__ = "John Bjorn Nelson"
-__email__ = "jbn@abreka.com"
-
 PEAK = 1
 VALLEY = -1
